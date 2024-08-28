e9a60ce2a58aeb51dde1d07aae1e780dda23a56f
badyss portable <badyss572@gmail.com>
2024-08-28 20:09:30 +0200

    test

--- exec.py
+++ exec.py
@@ -1,11 +1,11 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
+url = "https://jsonplaceholder.typicode.com/posts/1/comments"
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    print("Titre:", data["title"])
-    print("Contenu:", data["body"])
+    for comment in data:
+        print("Email:", comment["email"])
 else:
     print("Erreur lors de la récupération des données")
