3f2b6aa66139eea5f80d01d8e43a74a3dec37e78
badyss portable <badyss572@gmail.com>
2024-08-28 20:01:36 +0200

    oui

--- exec.py
+++ exec.py
@@ -1,1 +1,11 @@
-print("salut")
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Titre:", data["title"])
+    print("Contenu:", data["body"])
+else:
+    print("Erreur lors de la récupération des données")
