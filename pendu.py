37389b4e1a0ff5c68227c3057998325cbc958070
badyss portable <badyss572@gmail.com>
2024-09-04 18:43:32 +0200

    rejouer function

--- pendu.py
+++ pendu.py
@@ -2,30 +2,47 @@ import random
 
 mots = ["affichage", "reste", "manger"]
 
+def mot_trouve(mot, affichage):
+    for lettre in mot:
+        if lettre not in affichage:
+            return False
+    return True
+
 def main(mot, affichage):
-    affichage_mot = mot[0]
-    for lettre in mot[1:]:
-        if lettre in affichage:
-            affichage_mot += lettre
+    if mot_trouve(mot, affichage):
+        rejouer = input("Bravo tu as gagné, veux-tu rejouer ? (oui/non) : ")
+        if rejouer.lower() == "oui":
+            jouer()
         else:
-            affichage_mot += "_"
-    print(affichage_mot)
-    
+            print("D'accord, au revoir !")
+            return True 
+    else:
+        affichage_mot = mot[0]
+        for lettre in mot[1:]:
+            if lettre in affichage:
+                affichage_mot += lettre
+            else:
+                affichage_mot += "_"
+        print(affichage_mot)
+    return False
+
 def jouer():
     mot = random.choice(mots)
     affichage = []
     erreurs = 0
 
     while True:
-        main(mot, affichage)
-        tentative = input("Choississez une lettre")
+        if main(mot, affichage):
+            break
+        tentative = input("Choisissez une lettre : ")
         if tentative in mot:
             if tentative in affichage:
-                print("La lettre à déjà été trouvée")
+                print("La lettre a déjà été trouvée.")
             else:
                 affichage.append(tentative)
                 print("Trouvé !")
         else:
             erreurs += 1
             print("Non, tu as " + str(erreurs) + " erreurs.")
+
 jouer()
